e018ec01f24af34bfef488354a8b05a135c72e4f
Evan Tedesco <Evantedesco@gmail.com>
2018-02-08 08:13:14 -0700

    Setup campaigns/:address route

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Card, Button} from 'semantic-ui-react';
 import Layout from '../components/Layout';
 import factory from '../ethereum/factory';
+import {Link} from '../routes';
 
 class CampaignIndex extends Component {
 
@@ -15,7 +16,11 @@ class CampaignIndex extends Component {
     const items = this.props.campaigns.map(address => {
       return {
         header : address,
-        description : <a>View Campaign</a>,
+        description : (
+          <Link route={`/campaigns/${address}`}>
+          <a>View Campaign</a>
+          </Link>
+        ),
         fluid : true
       };
     });
@@ -24,20 +29,24 @@ class CampaignIndex extends Component {
 
 
   render() {
-    //TODO undo janky link tag
-    return(
-    <Layout>
-      <div>
-        <h3> Open Campaigns </h3>
-        <Button
-          floated="right"
-          content="Create Campaign"
-          icon="add circle"
-          primary
-        />
-        {this.renderCampaigns()}
-      </div>
-    </Layout>
+    return (
+      <Layout>
+        <div>
+          <h3> Open Campaigns </h3>
+          <Link route="/campaigns/new">
+            <a>
+              <Button
+                floated="right"
+                content="Create Campaign"
+                icon="add circle"
+                primary
+              />
+            </a>
+          </Link>
+
+          {this.renderCampaigns()}
+        </div>
+      </Layout>
     );
   }
 }
--- routes.js
+++ routes.js
@@ -1,4 +1,6 @@
 const routes = require('next-routes')();
 
-
+routes.add('/campaigns/new', 'campaigns/new');
+routes.add('/campaigns/:address', '/campaigns/show');
+routes.add('/campaigns', '/');
 module.exports = routes;
